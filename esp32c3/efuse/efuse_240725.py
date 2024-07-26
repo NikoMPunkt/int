e81e9903a213dec805780b0c2afc4323a5ffa2e2
NikoMPunkt <100423821+NikoMPunkt@users.noreply.github.com>
2024-07-26 15:32:28 +0200

    Update efuse_240725.py

--- esp32c3/efuse/efuse_240725.py
+++ esp32c3/efuse/efuse_240725.py
@@ -34,6 +34,7 @@ __BLOCK_LEN = (6,6,8,8,8,8,8,8,8,8,8)
 class EFUSE(__rs4432.RS):
     def __init__(self):
         super().__init__()
+        block = 0
     
     def Register(self, addr, pos, length, val = None):
         ''' set and get register '''
@@ -57,9 +58,43 @@ class EFUSE(__rs4432.RS):
         s = f'{self.Register(reg, 0, 32):032b}'
         return sep.join(s[i:i+every] for i in range(0, len(s), every))
     
-    # PGM_DATA
-    def Print_PGM_DATA(self):
-        ''' Get PGM_DATA '''
+    # 1 READ BLOCKS
+    # 1.1
+    def Read_blocks(self):
+        ''' Read ALL bloack at once into  DATA READ REGISTERS '''
+        # Read blocks
+        self.Register(EFUSE_CONF, 0, 1, CMD_READ)  # Set read command
+        self.Register(EFUSE_CMD, 0, 1, 1)          # send read command
+        while self.Register(EFUSE_CMD, 0, 1):
+            '''  check read_done interrupt '''
+            __sleep_ms(10)
+    
+    # 1.2
+    def Copy_block(self, block_nr=11):
+        ''' Copy block from DATA_READ REGISTER to PGM_DATA registers '''
+        assert block_nr<=10, 'Block number (0~10) needed!'
+        self.Clr_PGM_DATA_registers()
+        block_base_addr = READ_DATA_REGISTER + 4*(sum(__BLOCK_LEN[:block_nr]))
+        # Print blocks
+        for i in range(__BLOCK_LEN[block_nr]):
+            addr_block = block_base_addr + (i<<2)
+            addr_pgm  =  PGM_DATA_REGISTER + (i<<2)
+            __mem32[addr_pgm] = __mem32[addr_block]
+            #print(f'{i} Reg_0x{addr:04x}: 0x{self.Register(addr, 0, 32):08x} {self.Register_print(addr)}')
+        print(f'BLOCK {block_nr} copied to PGM_DATA')
+    
+    # 1.3
+    def Print_block(self, block_nr=11):
+        assert block_nr<=10, 'block number max 10!'
+        ''' Print DATA_READ REGISTER '''
+        block_base_addr = READ_DATA_REGISTER + 4*(sum(__BLOCK_LEN[:block_nr]))
+        print(f'BLOCK {block_nr} starting at 0x{block_base_addr:08x}:')
+        for i in range(__BLOCK_LEN[block_nr]):
+            addr_block = block_base_addr + (i<<2)
+            print(f'{i} Reg_0x{addr_block:04x}: 0x{self.Register(addr_block, 0, 32):08x} {self.Register_print(addr_block)}')
+    
+    # 1.4
+    def Print_PGM_DATA_registers(self):
         print(f'PGM_DATA registers:')
         for i in range(8):
             addr_pgm = PGM_DATA_REGISTER + (i<<2)
@@ -69,22 +104,41 @@ class EFUSE(__rs4432.RS):
             addr_chk = PGM_CHECK_REGISTER + (i<<2)
             print(f'{i} Reg_0x{addr_chk:04x}:  0x{self.Register(addr_chk, 0, 32):08x} {self.Register_print(addr_chk)}')
     
-    def Set_PGM_DATA(self, reg_nr, val):
+    # 2: Manipulating PGM_DATA
+    # 2.1
+    def Set_PGM_DATA_register(self, reg_nr, val):
         assert reg_nr<=7, 'Wrong register number!'
         __mem32[PGM_DATA_REGISTER + (reg_nr<<2)] = val & 0xffff_ffff
         self.Set_PGM_CHECK()
     
-    def Get_PGM_DATA(self, reg_nr=10):
+    # 2.2
+    def Get_PGM_DATA_register(self, reg_nr=10):
         assert reg_nr<=7, 'Wrong or no register number!'
         return __mem32[PGM_DATA_REGISTER + (reg_nr<<2)]
-          
-    def Clr_PGM_DATA(self):
+      
+    # 2.3 
+    def Clr_PGM_DATA_registers(self):
         ''' Clear PGM_DATA '''
         print(f'Clear PGM_DATA')
         for i in range(11):
             __mem32[PGM_DATA_REGISTER + (i<<2)] = 0
+            
+    # 3 PROGRAMM BLOCK
+    # 3.1
+    def Programm_block(self, block_nr=11):
+        ''' copy block to PGM_DAT '''
+        assert block_nr<=10, 'block number max 10!'
+        self.Register(EFUSE_CMD, 2, 4, block_nr)   # Set EFUSE_BLK_NUM to block number
+        self.Set_PGM_CHECK() 
+        self.Register(EFUSE_CONF, 0, 16, CMD_PROGRAMMING) # Command programming
+        self.Register(EFUSE_CMD, 1, 1, 1)          # send programming command
+        while self.Register(EFUSE_CMD, 0, 1):
+            '''  check read_done interrupt '''
+            __sleep_ms(10)
+        self.Clr_PGM_DATA()
+        print(f'Block {block_nr} programmed!')
     
-     # RS(44,32 check):
+    # 3.2 RS(44,32 check):
     def Set_PGM_CHECK(self):
         ''' set the PGM_CHECK_REGISTER acconding to PGM_DATA data '''
         data = 32*[0]
@@ -104,44 +158,7 @@ class EFUSE(__rs4432.RS):
                 value += (check[pos]<<shift)
             __mem32[addr] = value
     
-    # BLOCK_DATA
-    def Get_block(self, block_nr=0):
-        ''' Get BLOCK by mem32 command '''
-        assert block_nr<=10, 'block number max 10!'
-        self.__block_read()
-        block_base_addr = READ_DATA_REGISTER + 4*(sum(__BLOCK_LEN[:block_nr]))
-        print(f'BLOCK {block_nr} at 0x{block_base_addr:08x}')
-        for i in range(__BLOCK_LEN[block_nr]):
-            addr = block_base_addr + (i<<2)
-            print(f'{i} Reg_0x{addr:04x}: 0x{self.Register(addr, 0, 32):08x} {self.Register_print(addr)}')
-            
-    
-    # Shift
-    def __block_read(self):
-        ''' Read all blocks at once! '''
-        self.Register(EFUSE_CONF, 0, 16, CMD_READ) # Set read command
-        #self.Register(EFUSE_INT_ENA, 0, 1, 1)     # set signal for read_done interrupt
-        self.Register(EFUSE_CMD, 0, 1, 1)          # send read command
-        while self.Register(EFUSE_CMD, 0, 1):
-            '''  check read_done interrupt '''
-            __sleep_ms(10)
-        #self.Register(EFUSE_INT_CLR, 0, 1, 1)      #  clear signal for read_done interrupt
-            
-    
-    def Programm_block(self, block_nr=0):
-        ''' copy block to PGM_DAT '''
-        assert block_nr<=10, 'block number max 10!'
-        self.Register(EFUSE_CMD, 2, 4, block_nr)   # Set EFUSE_BLK_NUM to block number
-        self.Set_PGM_CHECK() 
-        self.Register(EFUSE_CONF, 0, 16, CMD_PROGRAMMING) # Command programming
-        #self.Register(EFUSE_INT_ENA, 1, 1, 1)      # set signal for read_done interrupt
-        self.Register(EFUSE_CMD, 1, 1, 1)          # send programming command
-        while self.Register(EFUSE_CMD, 0, 1):
-            '''  check read_done interrupt '''
-            __sleep_ms(10)
-        self.Clr_PGM_DATA()
-        #self.Register(EFUSE_INT_CLR, 1, 1, 1)      #  clear signal for read_done interrupt
-        
+    # 3.3 
     def Print_Report(self):
         ''' did an error occured? '''
         print('ERROR codes')
@@ -153,4 +170,5 @@ class EFUSE(__rs4432.RS):
             addr = EFUSE_RD_RS_ERRn + (i<<2)
             print(f'BLOCK1-10 at 0x{addr:04x}: 0x{self.Register(addr, 0, 32):08x} - b{self.Register_print(addr)}', end=' ')
             print('OK') if not __mem32[addr] else print('error')
+
 
